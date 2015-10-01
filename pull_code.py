f39830ef7c93f81ea19a25d2eef9e1974528517e
Your Name <you@example.com>
2015-10-01 20:40:17 +0530

    October 01 20:40 hours

--- pull_code.py
+++ pull_code.py
@@ -6,7 +6,7 @@ time = datetime.now().strftime("%B %d %H:%M hours")
 status = "git status"
 add = 'git add .'
 commit = "git commit -a -m " + "\"" +  time + "\""
-pull = "git pull -u code master"
+pull = "git pull -u code master < passwd.txt"
 os.system(status)
 proceed = raw_input('Proceed? (y/n): ')
 if (proceed == 'y'):
